cbc4ac439dce66cacc2e21a45c7e3c6e6776d48d
Cpietro87 <cristianpietrobon2019@gmail.com>
2026-04-14 22:17:50 -0300

    props y componentes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Saludar from './components/Saludar'
 import Producto from './components/Producto'
 import Contador from './components/Contador'
 import TarjetaUsuario from './components/TarjetaUsuario'
+import Button from '@mui/material/Button';
+import Pagination from '@mui/material/Pagination';
 
 function App() {
   
@@ -18,9 +20,18 @@ function App() {
     <Contador />
     <Contador />
 
+
     <TarjetaUsuario nombre="María" edadInicial={25} />
     <TarjetaUsuario nombre="Juan" edadInicial={30} />
 
+    <Button variant="text">Text</Button>
+    <Button variant="contained">Contained</Button>  
+    <Button variant="outlined">Outlined</Button>
+
+    <Pagination count={10} />
+    <Pagination count={10} color="primary" />
+    <Pagination count={10} color="secondary" />
+    <Pagination count={10} disabled />
 
   </>
 
---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+function Button() {
+    return (  
+    
+    <>
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+    </>);
+}
+
+export default Button;
